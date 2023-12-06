e734d1c7d802fff1943d63f25665711814463523
HamidReza <delshad.swdn@gmail.com>
2023-12-07 01:43:09 +0330

    add hotels component and filter based on search params

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,14 +1,22 @@
 import { useSearchParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import Loader from "../Loader/Loader";
 
 export default function Hotels() {
   const [searchParams, setSearchParams] = useSearchParams();
-const destination = searchParams.get("destination")
+  const destination = searchParams.get("destination");
+  const room = JSON.parse(searchParams.get("options"))?.room;
 
-console.log(destination);
+  const { isLoading, data } = useFetch(
+    "http://localhost:5000/hotels",
+    `q=${destination || ""}&accommodates_gte=${room || 1}`
+  );
+  //name_like, host_location_like
+  if (isLoading) <Loader />;
 
   return (
     <div>
-      <h1>hotels</h1>
+      <h1>{data.length}</h1>
     </div>
   );
 }
